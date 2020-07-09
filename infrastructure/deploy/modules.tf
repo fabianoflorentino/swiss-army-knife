36c4116078eb092b4b1c985c74f534217e2f6124
Fabiano Florentino <fabianoflorentino@outlook.com>
2020-07-09 13:39:22 -0300

    INFRASTRUCTURE/DEPLOY: - Remove .terraform - Ajust syntax on module.tf GITIGNORE: - Ajust for infrastructure/deploy

--- infrastructure/deploy/modules.tf
+++ infrastructure/deploy/modules.tf
@@ -1,10 +1,9 @@
 # module "sample" {
-#   source = "./modules/sample"
-  
-#   vm_count      = "1"
-#   name_new_vm   = "sample-node"
-#   num_cpus      = "1"
-#   num_mem       = "512"
-#   size_disk     = "70"
+#     source = "./modules/sample"
 
+#     vm_count      = "1"
+#     name_new_vm   = "sample-node"
+#     num_cpus      = "1"
+#     num_mem       = "512"
+#     size_disk     = "70"
 # }
